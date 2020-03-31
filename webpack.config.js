843e8804300cc34a2ebd95da526a613334929095
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-03-31 14:28:42 +0300

    Added form element as wrapper for all inputs

--- webpack.config.js
+++ webpack.config.js
@@ -60,13 +60,13 @@ module.exports = {
       inject: true
     }),
     */
-    /*
+    
     new HtmlWebpackPlugin({
       template: './src/pug/pages/Headers&Footers.pug',
       filename: './index.html',
       inject: true
     }),
-    */
+    
     /*
     new HtmlWebpackPlugin({
       template: './src/pug/pages/cards.pug',
@@ -75,13 +75,13 @@ module.exports = {
     }),
   */
 
-  
+/*  
     new HtmlWebpackPlugin({
       template: './src/pug/pages/form-elements.pug',
       filename: './index.html',
       inject: true
     }),
-  
+  */
     /*
     new HtmlWebpackPlugin({
       template: './src/pug/pages/colors&types.pug',
