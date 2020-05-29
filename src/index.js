3db586c4c1ec4094ca9e12e0df55d842268ab0b9
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-05-29 13:10:15 +0300

    Installed owl carousel and connected it to room component.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
 function importAll(resolve) {
   resolve.keys().forEach(resolve);
 }
 
 importAll(require.context('./', true, /\.js$|\.scss$|\.svg$|\.css$/));
 
+$(document).ready(function(){
+  const id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  id.forEach((id) => {
+    $(`#carousel-${id}`).owlCarousel({
+      items: 1,
+      nav: true,
+      navContainer: `#controls-${id}`,
+      dots: true,
+      dotsContainer: `#dots-${id}`,
+    });
+  })
+
+});
