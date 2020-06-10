03f78ac5bac45d4ae4ff5c177bb2de96ac147798
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-06-10 17:57:00 +0300

    Changed naming style of components to classical naming convention, in remaining components #6.

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -2,24 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function onCheckboxChangeHandler(e) {
     let parent = e.target.parentNode.parentNode;
-    let checkboxes = parent.querySelectorAll('.js-rate_button__checkbox');
+    let checkboxes = parent.querySelectorAll('.js-rate-button__checkbox');
     let ar = Array.from(checkboxes);
     let number = e.target.name;
     
 
     checkboxes.forEach((element, index) => {
-      if(index < number && !element.classList.contains('rate_button__sprite--checkbox-checked')) {
-        element.nextElementSibling.children[0].classList.add('rate_button__sprite--checkbox-checked');
+      if(index < number && !element.classList.contains('rate-button__sprite--checkbox-checked')) {
+        element.nextElementSibling.children[0].classList.add('rate-button__sprite--checkbox-checked');
       }
       else {
-        element.nextElementSibling.children[0].classList.remove('rate_button__sprite--checkbox-checked');
+        element.nextElementSibling.children[0].classList.remove('rate-button__sprite--checkbox-checked');
       }
     });
 
-    parent.querySelector('.js-rate_button__counter').value = number;
+    parent.querySelector('.js-rate-button__counter').value = number;
   }
 
-  let checkboxes = document.querySelectorAll('.js-rate_button__checkbox')
+  let checkboxes = document.querySelectorAll('.js-rate-button__checkbox')
 
   let checkboxesAr = Array.from(checkboxes);
 
