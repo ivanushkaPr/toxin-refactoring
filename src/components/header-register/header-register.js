27dce0f3da1f4d8b9e4c3908d2cee110ba82b963
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-06-23 18:27:29 +0300

    Rewrited header-register.js code into class #8.

--- src/components/header-register/header-register.js
+++ src/components/header-register/header-register.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('.header_register__burger_menu').click(function() {
-    $('.header__menu').toggle(300);
-  })
+  class BurgerMenu {
+    constructor(openButtonClassName, containerClassName) {
+      this.init(openButtonClassName, containerClassName)
+    }
+
+    init(openButtonClassName, containerClassName) {
+      $(openButtonClassName).click(function() {
+        $(containerClassName).toggle(300);
+      })
+    }
+  }
+
+  const burgerMenu = new BurgerMenu('.header-register__burger-menu', '.header-register__menu');
 })
