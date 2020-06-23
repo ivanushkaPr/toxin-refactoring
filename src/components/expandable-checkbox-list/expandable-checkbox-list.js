ba55fe5bd7684bd05088533bc3cb7939c8d214f3
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-06-23 17:57:41 +0300

    Rewrited code in expandable-checkbox-list.js into class #8.

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-  $('.js-expandable-checkbox-list__button-open').click(function() {
-    const component_root = $(this).parent().parent();
-    $(this).toggleClass('expandable-checkbox-list__button-open_opened');
-    $('.js-expandable-checkbox-list__list', component_root).toggle(300);
-  })
+  class ExpandableCheckBoxList {
+    constructor(buttonClassName) {
+      this.button = $(buttonClassName);
+      this.root = this.button.parent().parent();
+      this.init();
+    }
+
+    init() {
+      const that = this;
+      that.button.click(function() {
+        that.toggleButtonClass.call(this);
+        that.toggleCheckboxList();
+      });
+    }
+
+    toggleButtonClass() {
+      $(this).toggleClass('expandable-checkbox-list__button-open_opened');
+    }
+
+    toggleCheckboxList() {
+      $('.js-expandable-checkbox-list__list', this.root).toggle(300);
+    }
+  }
+
+  let expandable = new ExpandableCheckBoxList('.js-expandable-checkbox-list__button-open');
 });
